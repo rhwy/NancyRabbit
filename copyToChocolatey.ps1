1ce26731cabcfe332f30bc64bfc7cb324a467d6f
Rui Carvalho <github@rui.fr>
2012-10-17 03:07:36 +0200

    adding a small script to install debug bins to chocolatey path (in order to get it accessible from the PATH)

---
+++ copyToChocolatey.ps1
@@ -0,0 +1,28 @@
+﻿$chocolatey = "C:\Chocolatey"
+$libName = "rabbit2"
+$libfolder = "$chocolatey\lib\$libname"
+$libBin = "$chocolatey\bin\rabbit.bat"
+$currentPath =  $myinvocation.mycommand.path | split-path -parent
+if(Test-Path $chocolatey)
+{
+    #create folder for library if not exists
+    if(-not(Test-Path $libfolder))
+    {
+        new-item $libfolder -ItemType "Directory"
+    }
+
+    #copy files to library
+    Copy-Item -Path $currentPath\Src\rabbit\bin\Debug\*.* -Destination $libfolder -Force
+
+    #generat bat file for the library if not exist
+    if(-not(Test-Path $libBin))
+    {
+    $bat = 
+"@echo off
+SET DIR=%~dp0%
+`"%DIR%..\lib\$libName\rabbit.exe`" %*"
+    
+    $bat | Out-File $libBin -Force ascii
+    }
+    
+}
