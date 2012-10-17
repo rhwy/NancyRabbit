ffe057d96a24a5e800af6a055673e552fac138d4
Rui Carvalho <github@rui.fr>
2012-10-17 16:16:56 +0200

    include a sample folder with a nancy module file and a static file
    run rabbit.exe in this folder to test

---
+++ Samples/SimpleSite/app.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Nancy;
+
+public class HomeModule : NancyModule
+{
+    public HomeModule()
+    {
+        Get["/api/version"] = p => "version 0.1.4.7";
+        Get["/default.html"] = p => View["default"];
+        Get["/{path}"] = p => {
+            return string.Format("Hello from Nancy! <br/> path : {0}", string.IsNullOrWhiteSpace(p.path) ? "/" : p.path);
+        };
+
+    }
+}
